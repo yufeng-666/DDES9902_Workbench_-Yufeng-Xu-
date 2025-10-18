a36d8dd918d1aec762e7a67c79a59db93724db3a
yufeng-666 <2391737049@qq.com>
2025-10-19 03:46:27 +1100

    improve

--- Assets/__Assessment1_Assets/Scenes/CheckCarrotCount.cs
+++ Assets/__Assessment1_Assets/Scenes/CheckCarrotCount.cs
@@ -3,13 +3,13 @@ using UnityEngine.Events;
 
 public class CheckCarrotCount : MonoBehaviour
 {
-    public CarrotCounter carrotCounter;
+    public CarrotCounter carrotCounter; 
     public int targetCount = 3;
     public UnityEvent onCorrect;
-    public UnityEvent onWrong;
-
+    public UnityEvent onWrong; 
     public void CheckCount()
     {
+        Debug.Log("current number £º" + carrotCounter.currentCount); 
         if (carrotCounter.currentCount == targetCount)
             onCorrect.Invoke();
         else
